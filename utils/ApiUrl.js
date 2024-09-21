1cfc40728d04bd42e36cc3239a5c842e64849ff5
Abdur Razzak <abdur35-2672@diu.edu.bd>
2024-09-21 14:27:15 +0300

    na

--- utils/ApiUrl.js
+++ utils/ApiUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:4001/api/v1/admin/"
+const baseUrl = "http://localhost:3012/api/v1/program/admin/private/"
 const ApiUrl = {
   CREATE_CATEGORY: baseUrl + 'course_category',
   GET_CATEGORY_LIST: baseUrl + 'course_category',
